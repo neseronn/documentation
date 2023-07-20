35740bd5c204b15973acba75b498d79151d64470
Авдеенко Елизавета ИВТ-301 <avdeenko.l26@mail.ru>
2023-07-20 04:04:13 +0300

    feat: added store provider, realised output of received articles.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,22 @@
+import { useDispatch } from 'react-redux';
 import './App.css';
 import ArticleItem from './components/ArticleItem';
 import Categories from './components/Categories';
+import { fetchArticles } from './store/articlesSlice';
+import { useEffect } from 'react';
+import { AppDispatch} from './store/store';
+import { useTypedSelector } from './hooks/useTypedSelector';
 
 function App() {
+  const dispatch = useDispatch<AppDispatch>();
+  const { articles, error, isLoading } = useTypedSelector(
+    (state) => state.articles
+  );
+
+  useEffect(() => {
+    dispatch(fetchArticles());
+  }, [dispatch]);
+
   return (
     <>
       <div className='filters'>
@@ -12,8 +26,17 @@ function App() {
       <div className='content'>
         <div className='articles-list'>
           <h1>Нуждаются в популяризации</h1>
-          <ArticleItem tags={['CMS', 'Диагностика', 'DNS']} />
-          <ArticleItem tags={['CMS', 'Диагностика', 'DNS']} />
+
+          {isLoading ? (
+            <div>LOADING...</div>
+          ) : error ? (
+            <div>{error}</div>
+          ) : (
+            articles &&
+            articles.map((article) => (
+              <ArticleItem article={article} key={article.id} />
+            ))
+          )}
         </div>
       </div>
     </>
