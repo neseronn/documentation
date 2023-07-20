c376fa6c08ab4ab7075f2284ad846b74abfb0981
Авдеенко Елизавета ИВТ-301 <avdeenko.l26@mail.ru>
2023-07-20 21:34:53 +0300

    feat: added component Tags, changed Tag props and styles.

--- frontend/src/components/Tag.tsx
+++ frontend/src/components/Tag.tsx
@@ -1,12 +1,13 @@
 import { FC } from 'react';
-import style from '../styles/tag.module.css'
+import style from '../styles/tag.module.css';
 
 interface ITagProps {
   tagName: string;
+  isBtn?: boolean;
 }
 
-const Tag: FC<ITagProps> = ({ tagName }) => {
-  return <div className={style.tag}>{tagName}</div>;
+const Tag: FC<ITagProps> = ({ tagName, isBtn = false }) => {
+  return <div className={style.tag + (isBtn ? ' tag-btn' : '')}>{tagName}</div>;
 };
 
 export default Tag;
---
+++ frontend/src/components/Tags.tsx
@@ -0,0 +1,18 @@
+import Tag from './Tag';
+
+const tags = ['CMS', 'Bitrix', 'Диагностика', 'DNS', 'FTP', 'Crontab'];
+
+const Tags = () => {
+  return (
+    <div>
+      <h3>Тэги:</h3>
+      <div className='tag-list'>
+        {tags.map((tag) => (
+          <Tag key={tag} tagName={tag} isBtn={true}/>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Tags;
