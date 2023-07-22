03208b61d6b1157894705205b3d969940dfb65b6
= <2003lesha2003@mail.rum>
2023-07-22 23:44:19 +0300

    feat: add sort and search for param in route

--- api/common.py
+++ api/common.py
@@ -1,7 +1,7 @@
-from .models import Articles
 from django.forms.models import model_to_dict
-from .serializers import *
 
+from .serializers import *
+from .models import Articles
 
 def is_sublist(sublist, mainlist):
     return all(item in mainlist for item in sublist)
@@ -10,38 +10,32 @@ def responseDataAboutOneRecord(id):
     article_data = Articles.objects.get(id=id)
     dict_article_data = model_to_dict(article_data)
     all_content = list()
+    
     for i in dict_article_data['content']:
         all_content.append(DataContentArticlesSerializer(i).data)
+        
     datas = DataArticlesSerializer(article_data).data
     datas['content'] = all_content
     return datas
 
-def responseAllDataAndFilterAboutCategoryAndTags(category, tags):
-    if category is None:
-        if tags is None:
-            allData = Articles.objects.all()
-            serializers = ShortDataArticlesSerializer(allData, many=True)
-            return serializers.data
-        else:
-            tags = tags.split(',')
-            allData = Articles.objects.all()
-            serializers = ShortDataArticlesSerializer(allData, many=True)
-            responseData = list()
-            for i in serializers.data:
-                if (is_sublist(tags,i['tags'])):
-                    responseData.append(i)
-            return responseData
-    else:
-        if tags is None:
-            allData = Articles.objects.filter(category=category)
-            serializers = ShortDataArticlesSerializer(allData, many=True)
-            return serializers.data
-        else:
-            tags = tags.split(',')
-            allData = Articles.objects.filter(category=category)
-            serializers = ShortDataArticlesSerializer(allData, many=True)
-            responseData = list()
-            for i in serializers.data:
-                if (is_sublist(tags,i['tags'])):
-                    responseData.append(i)
-            return responseData
+def responseAllDataWithFilters(category, tags, search, sort):
+    allData = Articles.objects.all()
+    
+    if search is not None:
+        allData = allData.filter(title__icontains=search)
+    
+    if category is not None:
+        allData = allData.filter(category=category)    
+    if sort is not None:
+        allData = allData.order_by(f'-{sort}')
+    if tags is not None:
+        tags = tags.split(',')
+        serializers = ShortDataArticlesSerializer(allData, many=True)
+        responseData = list()
+        for i in serializers.data:
+            if (is_sublist(tags,i['tags'])):
+                responseData.append(i)
+        return responseData
+    
+    serializers = ShortDataArticlesSerializer(allData, many=True)
+    return serializers.data
--- api/views.py
+++ api/views.py
@@ -1,22 +1,25 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import *
-from .models import *
 from collections import OrderedDict
 from django.forms.models import model_to_dict
 from rest_framework.permissions import AllowAny
 
-from .common import responseDataAboutOneRecord, responseAllDataAndFilterAboutCategoryAndTags
+from .serializers import *
+from .models import *
+from .common import responseDataAboutOneRecord, responseAllDataWithFilters
 # Create your views here.
 class GetArticleView(APIView):
     permission_classes = (AllowAny,)
+    
     def get(self, request, id=None, format=None):
         if id != None:
             return Response(responseDataAboutOneRecord(id))
         else:
             category = request.GET.get('category', None)
+            search = request.GET.get('search', None)
             tags = request.GET.get('tags')
-            return Response(responseAllDataAndFilterAboutCategoryAndTags(category, tags))
+            sort = request.GET.get('sort')
+            return Response(responseAllDataWithFilters(category, tags, search, sort))
            
                 
            
